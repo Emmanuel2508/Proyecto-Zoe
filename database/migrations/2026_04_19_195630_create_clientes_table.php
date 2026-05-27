358b0c2df4ab769a2932dc02c215c9816000cf7f
Emmanuel2508 <23151202@aguascalientes.tecnm.mx>
2026-05-27 17:57:12 -0600

    Se agrego el rasgo de contraseña a la tabla cliente

--- database/migrations/2026_04_19_195630_create_clientes_table.php
+++ database/migrations/2026_04_19_195630_create_clientes_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string("direccion");
             $table->string("email")->unique();
             $table->string("telefono");
+            $table->string("contraseña");
         });
     }
 
