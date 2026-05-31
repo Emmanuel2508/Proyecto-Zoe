65a786313ac25d92a49e12e0f9b92710aec70dfa
Emmanuel2508 <23151202@aguascalientes.tecnm.mx>
2026-05-31 12:23:27 -0600

    Actualizado para que no se muestre el valor hash en el formulario modificar cliente

--- resources/views/Clientes/clientemodificar.blade.php
+++ resources/views/Clientes/clientemodificar.blade.php
@@ -20,7 +20,7 @@
         <input type="number" name="telefono" id="telefono" required value="{{$cliente->telefono}}"
         class="border-1">
         Contraseña:
-        <input type="text" name="contraseña" id="contraseña" required
+        <input type="text" name="contraseña" id="contraseña"
         class="border-1">
         <button type="submit" class="border-2">Modificar perfil</button>
     </form>
